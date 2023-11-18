8fc4213a6cb27caf37e3c7e6ebc04e3c46d96f83
New20Space <new20space.@gmail.com>
2023-11-19 01:17:09 +0300

    1

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
-
-echo  'https://new20space.vercel.app'.$url . '.php';
+$content = file_get_contents('https://new20space.vercel.app'. $url . '.php');
+echo  $content;
 ?>
