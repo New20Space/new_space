d73bccf184fe3ec50acbc166db3b9610744a8405
New20Space <new20space.@gmail.com>
2023-11-19 18:20:13 +0300

    1

--- api/index.php
+++ api/index.php
@@ -1,8 +1,17 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
 
+$arr = require 'pages.php';
+
+
+$e;
+
+foreach ($arr as $elem) {
+	$e=$elem+$e;
+}
+
 $layout = file_get_contents('layout.html');
-$content = file_get_contents('https://new20space.vercel.app/api/1.php');
+$content = file_get_contents('https://new20space.vercel.app/api/1.php')+$e;
 $layout = str_replace('{{ content }}', $content, $layout);
 echo  $layout;
 ?>
