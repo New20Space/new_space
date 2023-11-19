46b0a37d28948dfe3a8c63f6ffcbe5fc23039103
New20Space <new20space.@gmail.com>
2023-11-19 18:04:58 +0300

    1

--- api/1.php
+++ api/1.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <title>Заголовок страницы</title>
-    <link rel="stylesheet" href="./Styles/style.css">
-</head>
-<body>
-	<p>T2</p>
-   
-</body>
-</html>
+<div>
+	content 3
+</div>
--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
+
+$layout = file_get_contents('layout.html');
 $content = file_get_contents('https://new20space.vercel.app/api/1.php');
-echo  $content;
+$layout = str_replace('{{ content }}', $content, $layout);
+echo  $layout;
 ?>
