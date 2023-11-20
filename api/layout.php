1fa03188c40e884b5c14a458613b1dbc9a7026a2
New20Space <new20space.@gmail.com>
2023-11-20 19:33:55 +0300

    Layout

--- api/layout.php
+++ api/layout.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/api/style.css">
+    <link rel="stylesheet" href="./api/style.css">
     <title>New Space</title>
 </head>
 <body >
@@ -10,7 +10,7 @@
 		<div class="star"></div>
 		<div class="logo">
 			<a href="https://new20space.vercel.app/">
-				<img src="/api/Img/logo2.png"width="150" height="150">
+				<img src="./api/Img/logo2.png"width="150" height="150">
 			</a>	
 		</div>
 		<div class="m">
