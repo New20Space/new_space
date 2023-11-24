49806f1b441b841f96e4af261758fe7290b0a680
New20Space <new20space.@gmail.com>
2023-11-24 13:52:11 +0300

    9

--- layout.php
+++ layout.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>New Space</title>
 </head>
 <body >
@@ -10,7 +10,7 @@
 		<div class="star"></div>
 		<div class="logo">
 			<a href="https://new20space.vercel.app/">
-				<img src="Img/logo2.png"width="150" height="150">
+				<img src="./Img/logo2.png"width="150" height="150">
 			</a>	
 		</div>
 		<div class="m">
