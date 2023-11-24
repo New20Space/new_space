3e05b4849948fe64a4058be029ad00f129e0e1a2
New20Space <new20space.@gmail.com>
2023-11-24 14:44:27 +0300

    7

--- api/index.php
+++ api/index.php
@@ -1,15 +1,15 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
 
-$arr = require './api/pages.php';
+$arr = require 'pages.php';
 
 $e='';
 for ($i = count($arr) - 1; $i >= 0; $i--) {
     $e=  $arr[$i].$e;
 }
 
-$layout = file_get_contents('./api/layout.php');
-$content = file_get_contents('./api/1.php').$e;
+$layout = file_get_contents('/api/layout.php');
+$content = file_get_contents('/api/1.php').$e;
 $layout = str_replace('{{ content }}', $content, $layout);
 echo  $layout;
 ?>
